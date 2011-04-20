feef859a28d96aba422838d7c884ad9c46321d4c
oranie <t.narita0917+jibun@gmail.com>
2011-04-20 18:07:24 +0900

    new file:   log_mail_control.pm

---
+++ log_analyze/log_mail_control.pm
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+package Log_Mail_Controls;
+
+use strict;
+use warnings;
+use utf8;
+use Encode;
+use Email::MIME;
+use Email::MIME::Creator;
+use Email::Send;
+use IO::All;
+
+sub mail_send{
+    my $body = $_[0];
+    my $file = $_[1];
+    if ($file =~ m/on/){
+        $file = '/tmp/fast_log2db.log';
+        $file = io($file)->all;
+    }
+
+    my @parts = (
+         Email::MIME->create(
+            'attributes' => {
+            'content_type' => 'text/plain',
+            'charset'     => 'ISO-2022-JP',
+            'encoding'    => '7bit',
+            },
+            'body' => Encode::encode( 'iso-2022-jp', $body ),
+        ),
+         Email::MIME->create(
+            'attributes' => {
+            'content_type' => 'text/plain',
+            'charset'     => 'ISO-2022-JP',
+            'encoding'    => '7bit',
+            },
+            'body' => Encode::encode( 'iso-2022-jp', $file ),
+        )
+    );
+
+    my $mail = Email::MIME->create(
+        header => [
+            From    => 'root@example.com',
+            To      => 'fuga@example.co.jp',
+            Subject => Encode::encode('MIME-Header-ISO_2022_JP', 'バッチ処理速報'),
+        ],
+        parts => [@parts],
+    );
+
+    my $sender = Email::Send->new({mailer => 'Sendmail'});
+    $sender->send($mail);
+}
+
+
+return 1;
