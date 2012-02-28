c8165ac76b3be07c07e159297fa4496c24e3ec21
oranie <t.narita0917+jibun@gmail.com>
2012-02-28 21:14:54 +0900

    modified:   check_fluentd.rb

--- ruby/check_fluentd.rb
+++ ruby/check_fluentd.rb
@@ -1,6 +1,6 @@
 #!/usr/local/rvm/rubies/ruby-1.9.3-p125/bin/ruby -Ku
 #
-#example: ./check_fluentd.rb  -f /var/log/td-agent/access_log.20120223 -h 127.0.0.1 -c 600 -w 180
+#example: ./check_fluentd.rb -p /var/log/td-agent/ -f access_log -h 127.0.0.1 -c 600 -w 180
 #
 require 'optparse'
 require 'json'
@@ -9,15 +9,26 @@ require 'time'
 opt = OptionParser.new
 opts = Hash.new
 
-opt.on('-f VAL' " Nagios check log file name") {|v| opts["logfile"] = v}
+opt.on('-p VAL' " Nagios check log file path") {|v| opts["log_path"] = v}
+opt.on('-f VAL' " Nagios check log file name") {|v| opts["file_name"] = v}
 opt.on('-h VAL' " Check host_tag name") {|v| opts["host"] = v}
 opt.on('-c VAL' " Check the time difference, or more if critical threshold (sec)") {|v| opts["critical"] = v}
 opt.on('-w VAL' " Check the time difference, or more if warnings threshold (sec)") {|v| opts["warnings"] = v}
 
 opt.parse!(ARGV)
 
-def line_check(line,host_tag)
-    if line =~ /#{host_tag}/
+def get_new_logfile(path,logfile)
+    cmd = "find #{path} -name \"#{logfile}*\"| xargs ls -t| head -n 1"
+    new_log_file = open("|#{cmd}")do |fp|
+        new_log_file = fp.gets
+    end
+    File.exist?("#{new_log_file.chomp}") 
+
+    return new_log_file.chomp
+end
+
+def line_check(log_line,host_tag)
+    if log_line =~ /#{host_tag}/
         return 0
     end
 end
@@ -28,7 +39,7 @@ def log_line_parse(log_line)
 end
 
 def tac_check(opts)
-    log_file = opts["logfile"]
+    log_file = opts["log_file"]
     host_tag = opts["host"]
     now_time = Time.new
     now_time = now_time.to_i
@@ -42,11 +53,11 @@ def tac_check(opts)
             log_time = log_time.to_i
             #該当パラメータが出てくるまでに、ログの時間が閾値を越えると各処理を実行
             if log_time <= critical_threshhold 
-                print  "Critical !!\nLAST LOG #{line}"
+                print  "Critical !!\nLAST LOG #{log_file}\n #{line}"
                 exit 2
             elsif log_time <= warnings_threshhold
                 if line_check(line,host_tag)
-                    print  "Warnings!!!\n#{line}"
+                    print  "Warnings!!!\n#{log_file}\n #{line}"
                     exit 1
                 end
                 next
@@ -59,9 +70,15 @@ def tac_check(opts)
             end
         end
         #最後まで読んでも該当パラメータが無い場合もcritical
-        print  "Critical !!\nI read until the end...Not Found #{host_tag}\n"
+        print  "Critical !!\nI read until the end...Not Found #{log_file}\n #{host_tag}\n"
         exit 2
     end
 end
 
-tac_check(opts)
+
+begin
+    opts["log_file"] = get_new_logfile(opts["log_path"],opts["file_name"])
+    tac_check(opts)
+rescue => evar
+    raise "ERROR!!! #{evar}"
+end
