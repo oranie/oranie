e956512e10e9e01981bea0d0af9586ee96612e16
oranie <t.narita0917+jibun@gmail.com>
2013-11-14 17:17:29 +0900

    log add

--- perl/cacti_host_rewrite.pl
+++ perl/cacti_host_rewrite.pl
@@ -8,9 +8,15 @@ use DBIx::QueryLog;
 use Log::Minimal;
 use Data::Dumper;
 
+local $DBIx::QueryLog::OUTPUT = sub {
+    my %args = @_;
+    infof($args{sql});
+};
+local $Log::Minimal::LOG_LEVEL = "INFO";
+
 my $database = "cacti_test";
 
-my $d = "DBI:mysql:$database";
+my $d = "DBI:mysql:$database:127.0.0.1";
 my $u = "root";
 my $p = "";
 
@@ -26,41 +32,44 @@ my $update_hostname = "";
 my $host_select_sql = "select description,hostname from host;";
 my $host_update_sql = "UPDATE host_test SET description=?,hostname=? WHERE description=? AND hostname=?;";
 
+
 eval{
-    print "DB execute start\n";
-    query_commit_db($host_select_sql) ;
-    print "DB execute end\n";
-};
-if($@){
-    print "$@ DB COMMIT ERROR";
+    infof("DB execute start");
+    get_and_update_hostname($host_select_sql);
+    infof("DB execute end");
+};if($@){
+    critf("$@ DB COMMIT ERROR");
 }
 
-sub query_commit_db{
+sub get_and_update_hostname{
     my $sql = $_[0] or die "No Query!!!";
 
-    my $dbh = DBI->connect($d, $u, $p)
-        or die "DB Connect error $!";
-    my $sth;
+    eval{
+        my $dbh = DBI->connect($d, $u, $p)
+            or die "DB Connect error $!";
+        my $sth;
 
-    $sth = $dbh->prepare($sql) ;
-    $sth->execute or die "sql execute error $sql";
-    while(my @row = $sth->fetchrow_array) {
-        $master_description = $row[0];
-        $master_hostname = $row[1];
-        infof("master data | description:$master_description\thostname:$master_hostname");
-        $update_description = $master_description;
-        $update_description =~ s/$regexp_base/$regexp_change/;
-        $update_hostname = $update_description . $base_domain;
+        $sth = $dbh->prepare($sql) ;
+        $sth->execute or die "sql execute error $DBI::errstr";
+        while(my @row = $sth->fetchrow_array) {
+            $master_description = $row[0];
+            $master_hostname = $row[1];
+            infof("master data | description:$master_description\thostname:$master_hostname");
+            $update_description = $master_description;
+            $update_description =~ s/$regexp_base/$regexp_change/;
+            $update_hostname = $update_description . $base_domain;
 
-        my $update_sth = $dbh->prepare($host_update_sql);
-        $update_sth->execute($update_description,$update_hostname,$master_description,$master_hostname) or die "sql execute error $sql";
-        infof("update data | description:$update_description\thostname:$update_hostname"); 
+            my $update_sth = $dbh->prepare($host_update_sql);
+            $update_sth->execute($update_description,$update_hostname,$master_description,$master_hostname)
+                 or die "sql execute error $DBI::errstr";
+            infof("update data | description:$update_description\thostname:$update_hostname"); 
+        }
+        infof("DB execute OK!");
+        $sth->finish or die "DB Connection Close ERROR" ;
+        $dbh->disconnect;
+    };if($@){
+        critf($@);
     }
-    infof("DB execute OK!");
-    $sth->finish or die "DB Connection Close ERROR" ;
-    $dbh->disconnect;
-
     return 0;
 }
-
 
