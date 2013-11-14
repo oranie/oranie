543e8e4f1e9f18aae5550eab25573566db36ce89
oranie <t.narita0917+jibun@gmail.com>
2013-11-14 11:33:01 +0900

    regexp word add

--- perl/cacti_host_rewrite.pl
+++ perl/cacti_host_rewrite.pl
@@ -8,9 +8,15 @@ use DBIx::QueryLog;
 use Log::Minimal;
 use Data::Dumper;
 
-my $d = "DBI:mysql:cacti_test";
+my $database = "cacti_test";
+
+my $d = "DBI:mysql:$database";
 my $u = "root";
 my $p = "";
+
+my $base_domain = ".test.jp";
+my $regexp_base = "s13-soc-sb-";
+my $regexp_change = "sb-deka-soc-";
 
 my $master_description = "";
 my $master_hostname = "";
@@ -41,15 +47,16 @@ sub query_commit_db{
     while(my @row = $sth->fetchrow_array) {
         $master_description = $row[0];
         $master_hostname = $row[1];
+        infof("master data | description:$master_description\thostname:$master_hostname");
         $update_description = $master_description;
-        $update_description =~ s/s13-soc-sb-/sb-deka-soc-/;
-        $update_hostname = $update_description . ".test.jp";
+        $update_description =~ s/$regexp_base/$regexp_change/;
+        $update_hostname = $update_description . $base_domain;
+
         my $update_sth = $dbh->prepare($host_update_sql);
         $update_sth->execute($update_description,$update_hostname,$master_description,$master_hostname) or die "sql execute error $sql";
-        infof("master data | description:$master_description\thostname:$master_hostname");
         infof("update data | description:$update_description\thostname:$update_hostname"); 
     }
-    print "DB execute OK!\n";
+    infof("DB execute OK!");
     $sth->finish or die "DB Connection Close ERROR" ;
     $dbh->disconnect;
 
