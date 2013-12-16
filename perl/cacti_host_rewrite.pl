ebeb5de1922a25fe08905d77a1d6ba576e9c99ff
oranie <t.narita0917+jibun@gmail.com>
2013-12-16 13:35:32 +0900

    cluster_latency_check.pl 	hirose31_henkan.pl

--- perl/cacti_host_rewrite.pl
+++ perl/cacti_host_rewrite.pl
@@ -29,16 +29,48 @@ my $master_hostname = "";
 my $update_description = "";
 my $update_hostname = "";
 
-my $host_select_sql = "select description,hostname from host;";
-my $host_update_sql = "UPDATE host_test SET description=?,hostname=? WHERE description=? AND hostname=?;";
+my $host_select_sql = 
+    "select description,hostname from host;";
+my $backup_table_create_sql = 
+    "DROP TABLE IF EXISTS `host_test`;create table host_test LIKE host";
+my $backup_data_insert_sql = 
+    "INSERT INTO host_test (select * from host)";
+my $host_update_sql = 
+    "UPDATE host_test SET description=?,hostname=? WHERE description=? AND hostname=?;";
 
 
 eval{
+    create_backup($backup_table_create_sql,$backup_data_insert_sql);
     infof("DB execute start");
     get_and_update_hostname($host_select_sql);
     infof("DB execute end");
 };if($@){
     critf("$@ DB COMMIT ERROR");
+}
+
+sub create_backup{
+    my $backup_table_create_sql = $_[0];
+    my $backup_data_insert_sql = $_[1];
+
+    eval{
+        my $dbh = DBI->connect($d, $u, $p)
+            or die "DB Connect error $!";
+        my $sth;
+        infof("create host table backup start");
+        $sth = $dbh->prepare($backup_table_create_sql) ;
+        $sth->execute or die "sql execute error $DBI::errstr";
+        $sth = $dbh->prepare($backup_data_insert_sql) ;
+        $sth->execute or die "sql execute error $DBI::errstr";
+
+        $sth->finish or die "DB Connection Close ERROR";
+        $dbh->disconnect;
+        infof("create host table backup complete!!!");
+
+        return 0;        
+    }; if($@){
+        critf($@);
+        return 1;
+    }
 }
 
 sub get_and_update_hostname{
@@ -54,7 +86,7 @@ sub get_and_update_hostname{
         while(my @row = $sth->fetchrow_array) {
             $master_description = $row[0];
             $master_hostname = $row[1];
-            infof("master data | description:$master_description\thostname:$master_hostname");
+            debugf("master data | description:$master_description\thostname:$master_hostname");
             $update_description = $master_description;
             $update_description =~ s/$regexp_base/$regexp_change/;
             $update_hostname = $update_description . $base_domain;
@@ -62,14 +94,17 @@ sub get_and_update_hostname{
             my $update_sth = $dbh->prepare($host_update_sql);
             $update_sth->execute($update_description,$update_hostname,$master_description,$master_hostname)
                  or die "sql execute error $DBI::errstr";
-            infof("update data | description:$update_description\thostname:$update_hostname"); 
+            debugf("update data | description:$update_description\thostname:$update_hostname"); 
         }
         infof("DB execute OK!");
         $sth->finish or die "DB Connection Close ERROR" ;
         $dbh->disconnect;
+
+        return 0;
+
     };if($@){
         critf($@);
+        return 1;
     }
-    return 0;
 }
 
